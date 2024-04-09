2e3052f1e0bef5d4f2949acb53f2932b857119ef
Руслан <ryslann95@mail.ru>
2024-04-09 18:28:51 +0300

    мелкие исправления

--- js/windowRendering.js
+++ js/windowRendering.js
@@ -1,5 +1,3 @@
-/* eslint-disable no-shadow */
-/* eslint-disable no-unused-vars */
 import {
   photosData
 } from './createArrayPhotoData.js';
@@ -14,48 +12,55 @@ const bigPictureComments = document.querySelector('.social__comment-shown-count'
 const bigPictureCommentsAll = document.querySelector('.social__comment-total-count');
 const commentsNode = document.querySelector('.social__comments');
 const commentsNodeTemplate = commentsNode.querySelector('.social__comment');
-const commentsNodeCounter = commentsNode.querySelector('.social__comment-count');
-const commentsNodeLoader = commentsNode.querySelector('.comments-loader');
+const commentsNodeCounter = document.querySelector('.social__comment-count');
+const commentsNodeLoader = document.querySelector('.comments-loader');
 
-//функции открытия и закрытия большого изображения
-const openPhoto = (photosData) => {
-  // const currentPhoto =
-  bigPictureContainer.classList.remove('hidden');
-  bigPictureImage.src = photosData.url;
-  bigPicturelikes.textContent = photosData.likes;
-  const socialCommentsFragment = document.createDocumentFragment();
-  commentsNode.innerHTML = '';
+//Создаём фрагмент для комментария
+const socialCommentsFragment = document.createDocumentFragment();
 
-  //Не могу понять как вытащить комментарии из массива.
-  photosData.forEach((photo) => {
-    photo.comments.forEach((comment) => {
-      const socialCommentNode = commentsNode.cloneNode(true);
+//Функция создания комментария
+const createComments = (photoData) => {
+  photoData.comments.forEach((comment) => {
+    const socialCommentNode = commentsNodeTemplate.cloneNode(true);
 
-      socialCommentNode.querySelector('.social__picture').src = comment.avtar;
-      socialCommentNode.querySelector('.social__picture').alt = comment.name;
-      socialCommentNode.querySelector('.social__text').textContent = comment.message;
-      socialCommentsFragment.appendChild(socialCommentNode);
-    });
+    socialCommentNode.querySelector('.social__picture').src = comment.avatar;
+    socialCommentNode.querySelector('.social__picture').alt = comment.name;
+    socialCommentNode.querySelector('.social__text').textContent = comment.message;
+    socialCommentsFragment.appendChild(socialCommentNode);
   });
-
-  bigPictureComments.textContent = photosData.comments.length;
-  bigPictureCommentsAll.textContent = photosData.comments.length;
-
-  body.classList.add('.modal-open');
 };
-// Добавление комментария в DOM
 
+//функциz открытия большого изображения
+const openPhoto = (photoData) => {
+  bigPictureContainer.classList.remove('hidden');
+  bigPictureImage.src = photoData.url;
+  bigPicturelikes.textContent = photoData.likes;
+  commentsNode.innerHTML = '';
+  createComments(photoData);
+  commentsNode.appendChild(socialCommentsFragment);
+
+  bigPictureComments.textContent = photoData.comments.length;
+  bigPictureCommentsAll.textContent = photoData.comments.length;
+  commentsNodeCounter.classList.add('hidden');
+  commentsNodeLoader.classList.add('hidden');
+  body.classList.add('modal-open');
+};
+//Функция закрытия фото.
 const closePhoto = () => {
   bigPictureContainer.classList.add('hidden');
-
-  body.classList.remove('.modal-open');
+  body.classList.remove('modal-open');
 };
-
+//Закртытие фото по кнопке.
+document.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    bigPictureContainer.classList.add('hidden');
+    body.classList.remove('modal-open');
+  }
+});
+//Закрытие фото при клике.
 bigPictureClose.addEventListener('click', closePhoto);
 
 export {
   openPhoto
 };
-
-
-//Нужно чтобы при открытии большой картинки отображалось именно то изображение на которое мы кликнули
